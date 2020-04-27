071c263822aac838ee8a54f781fcf5b8d2170956
Tap Payments <o.rabie@tap.company>
2020-04-27 11:22:49 +0200

    1- Created the framework template. 2- Created the example project template

---
+++ TapUIKit-iOS/TapUIKit-Example/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TapUIKit-Example
+//
+//  Created by Osama Rabie on 27/04/2020.
+//  Copyright © 2020 Tap Payments. All rights reserved.
+//
+
+import UIKit
+import TapUIKit_iOS
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TapUIKit-iOS/TapUIKit-iOS/TapUIKit_iOS.h
@@ -0,0 +1,19 @@
+//
+//  TapUIKit_iOS.h
+//  TapUIKit-iOS
+//
+//  Created by Osama Rabie on 27/04/2020.
+//  Copyright © 2020 Tap Payments. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TapUIKit_iOS.
+FOUNDATION_EXPORT double TapUIKit_iOSVersionNumber;
+
+//! Project version string for TapUIKit_iOS.
+FOUNDATION_EXPORT const unsigned char TapUIKit_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TapUIKit_iOS/PublicHeader.h>
+
+
