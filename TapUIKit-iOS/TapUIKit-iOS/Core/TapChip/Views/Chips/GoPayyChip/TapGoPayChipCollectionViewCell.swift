e26d8d98edcca17b55f3f4bc0664c79de28810f1
O Rabie <o.rabie@tap.company>
2020-06-16 09:21:21 +0200

    1- GoPay viewmodel. 2- GoPay status enum. 3- GoPay statuses themes

--- TapUIKit-iOS/TapUIKit-iOS/Core/TapChip/Views/Chips/GoPayyChip/TapGoPayChipCollectionViewCell.swift
+++ TapUIKit-iOS/TapUIKit-iOS/Core/TapChip/Views/Chips/GoPayyChip/TapGoPayChipCollectionViewCell.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import TapThemeManager2020
 
 class TapGoPayChipCollectionViewCell: GenericTapChip {
 
@@ -16,5 +17,105 @@ class TapGoPayChipCollectionViewCell: GenericTapChip {
         super.awakeFromNib()
         // Initialization code
     }
+    
+    
+    public var viewModel:TapGoPayViewModel = .init() {
+        didSet{
+            viewModel.cellDelegate = self
+            reload()
+        }
+    }
+    
+    public func identefier() -> String {
+        return viewModel.identefier()
+    }
+    
+    override func configureCell(with viewModel: GenericTapChipViewModel) {
+        // Defensive coding it is the correct view model type
+        guard let correctTypeModel:TapGoPayViewModel = viewModel as? TapGoPayViewModel else { return }
+        self.viewModel = correctTypeModel
+    }
+    
+    public override func selectStatusChaned(with status:Bool) {
+        
+        // No theming required for ayment gatewy chip cell
+        return
+    }
+    
+    public override func tapChipType() -> TapChipType {
+        return .GoPayChip
+    }
+    
+    
+    /// The path to look for theme entry in
+    private var themePath:String {
+        get{
+            return tapChipType().themePath()
+        }
+    }
+    
+    // Mark:- Init methods
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    
+    /// Used as a consolidated method to do all the needed steps upon creating the view
+    private func commonInit() {
+        applyTheme()
+    }
+    
+    
+    public func reload() {
+        commonInit()
+    }
 
 }
+
+
+// Mark:- Theme methods
+extension TapGoPayChipCollectionViewCell {
+    /// Consolidated one point to apply all needed theme methods
+    public func applyTheme() {
+        matchThemeAttributes()
+    }
+    
+    /// Match the UI attributes with the correct theming entries
+    private func matchThemeAttributes() {
+        
+        tap_theme_backgroundColor = .init(keyPath: "\(themePath).backgroundColor")
+        layer.tap_theme_cornerRadious = .init(keyPath: "horizontalList.chips.radius")
+        
+        goPayLabel.tap_theme_font = .init(stringLiteral: "\(themePath).\(viewModel.tapGoPayStatus.themePath()).labelTextFont",shouldLocalise:false)
+        goPayLabel.tap_theme_textColor = .init(stringLiteral: "\(themePath).\(viewModel.tapGoPayStatus.themePath()).labelTextColor")
+    }
+    
+    /// Listen to light/dark mde changes and apply the correct theme based on the new style
+    override public func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        TapThemeManager.changeThemeDisplay(for: self.traitCollection.userInterfaceStyle)
+        applyTheme()
+    }
+}
+
+
+
+extension TapGoPayChipCollectionViewCell:TapGoPayViewModelDelegate {
+    
+    func changeGoPayStatus(with status: TapGoPayStatus) {
+        reload()
+    }
+    
+    func changeSelection(with status: Bool) {
+        selectStatusChaned(with: status)
+    }
+    
+    
+}
+
---
+++ TapUIKit-iOS/TapUIKit-iOS/Core/TapChip/Views/Chips/GoPayyChip/TapGoPayViewModel.swift
@@ -0,0 +1,54 @@
+//
+//  TapGoPayCollectionViewModel.swift
+//  TapUIKit-iOS
+//
+//  Created by Osama Rabie on 6/16/20.
+//  Copyright © 2020 Tap Payments. All rights reserved.
+//
+
+import Foundation
+
+internal protocol TapGoPayViewModelDelegate {
+    func changeSelection(with status:Bool)
+    func changeGoPayStatus(with status:TapGoPayStatus)
+}
+
+public class TapGoPayViewModel: GenericTapChipViewModel {
+    
+    
+    internal var cellDelegate:TapGoPayViewModelDelegate?
+    
+    public var tapGoPayStatus:TapGoPayStatus = .logIn {
+        didSet{
+            
+        }
+    }
+    
+    public override func identefier() -> String {
+        return "GoPayCollectionViewCell"
+    }
+    
+    
+    public override func didSelectItem() {
+        cellDelegate?.changeSelection(with: true)
+    }
+    
+    public override func didDeselectItem() {
+        cellDelegate?.changeSelection(with: false)
+    }
+}
+
+
+public enum TapGoPayStatus {
+    case logIn
+    case loggedIn
+    
+    func themePath() -> String {
+        switch self {
+        case .logIn:
+            return "logIn"
+        case .loggedIn:
+            return "loggedIn"
+        }
+    }
+}
