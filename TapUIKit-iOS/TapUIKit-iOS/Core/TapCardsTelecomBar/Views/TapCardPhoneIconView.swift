2a306cf1f4338459f0200f06bdf60bc3476c8e9a
O Rabie <o.rabie@tap.company>
2020-06-28 09:16:04 +0200

    1- Added the UI template for the card/phone icon view.

---
+++ TapUIKit-iOS/TapUIKit-iOS/Core/TapCardsTelecomBar/Views/TapCardPhoneIconView.swift
@@ -0,0 +1,22 @@
+//
+//  TapCardPhoneIconView.swift
+//  TapUIKit-iOS
+//
+//  Created by Osama Rabie on 6/28/20.
+//  Copyright © 2020 Tap Payments. All rights reserved.
+//
+
+import TapThemeManager2020
+
+class TapCardPhoneIconView: UIView {
+    @IBOutlet var contentView: UIView!
+    @IBOutlet weak var iconImageView: UIImageView!
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
