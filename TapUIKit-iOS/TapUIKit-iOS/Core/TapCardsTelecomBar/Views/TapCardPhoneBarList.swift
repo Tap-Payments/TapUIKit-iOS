6a868a3b94c10084cba3c67a12e185d75225c771
O Rabie <o.rabie@tap.company>
2020-06-29 07:46:13 +0200

    1- Created correct UIView for tabcardphone bar list. 2- Adjusting constraints

---
+++ TapUIKit-iOS/TapUIKit-iOS/Core/TapCardsTelecomBar/Views/TapCardPhoneBarList.swift
@@ -0,0 +1,21 @@
+//
+//  TapCardPhoneBarList.swift
+//  TapUIKit-iOS
+//
+//  Created by Osama Rabie on 6/29/20.
+//  Copyright © 2020 Tap Payments. All rights reserved.
+//
+
+import UIKit
+
+class TapCardPhoneBarList: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
