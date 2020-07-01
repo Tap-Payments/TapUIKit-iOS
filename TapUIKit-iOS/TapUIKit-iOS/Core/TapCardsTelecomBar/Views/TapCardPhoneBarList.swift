47794997b8e0c87fe2a19b4c37b8386f3394a262
O Rabie <o.rabie@tap.company>
2020-07-01 14:05:37 +0200

    1- TapCardPhoneBarList documentation

--- TapUIKit-iOS/TapUIKit-iOS/Core/TapCardsTelecomBar/Views/TapCardPhoneBarList.swift
+++ TapUIKit-iOS/TapUIKit-iOS/Core/TapCardsTelecomBar/Views/TapCardPhoneBarList.swift
@@ -11,25 +11,34 @@ import RxSwift
 import SimpleAnimation
 import EasyAnimation
 
+/// Represent the view of tab bar list
 public class TapCardPhoneBarList: UIView {
 
     /// Represents the content view that holds all the subviews
     @IBOutlet var contentView: UIView!
     /// Represents the holder layout for our icons horizontal bar views
     @IBOutlet weak var stackView: UIStackView!
+    /// Represents the leading constraint for the under line view
     @IBOutlet weak var underLineLeadingConstraint: NSLayoutConstraint!
+    /// Represents the width constraint for the under line view
     @IBOutlet weak var underLineWidthConstraint: NSLayoutConstraint!
+    /// Represents the under line view
     @IBOutlet weak var underLineBar: UIProgressView! {
         didSet{
+            // We need to make it changable regardless of the constraint
             underLineBar.translatesAutoresizingMaskIntoConstraints = false
         }
     }
     
+    /// Represents the max width the tabs can expand to
     internal var maxWidth:CGFloat = 50
     
+    /// Represents the View model that controls the actions and the ui of the card/phone tab bar
     internal var viewModel:TapCardPhoneBarListViewModel? {
         didSet{
+            // Once set, we delcare ourself as the view delegate for the view model
             viewModel?.viewDelegate = self
+            // We wire up the notifications from the view model to our channels
             bindObservables()
         }
     }
@@ -58,31 +67,38 @@ public class TapCardPhoneBarList: UIView {
         applyTheme()
     }
     
+    /// Used to bind all the needed reactive observables to its matching logic and functions
     private func bindObservables() {
         // Defensive coding to make sure there is a view model
         guard let viewModel = viewModel else { return }
+        // Listen to the change in the data source (the tabs to be rendered)
         viewModel.dataSourceObserver
             .distinctUntilChanged()
             .filter{ $0.count > 0 }
             .subscribe(onNext: { [weak self] (dataSource) in
+                // On a new data sourec, we need to reload our data
                 self?.relodData(with: viewModel.generateViews(with: self?.maxWidth ?? 60))
         }).disposed(by: disposeBag)
         
-        
+        // Listen to the change of the validation result of the selected tab
         viewModel.selectedIconValidatedObserver.distinctUntilChanged()
             .subscribe(onNext: { [weak self] (validated) in
+                // When the validation of the selection changes, we need to re theme our underline view
                 self?.themeBar(selectionValid: validated)
             }).disposed(by: disposeBag)
     }
     
-    
+    /**
+     Handles the logic for drawing the tabs
+     - Parameter views: The list of tabs we need to draw in the tab bar list view
+     */
     internal func relodData(with views:[TapCardPhoneIconView] = []) {
         // Hide the bar
         underLineBar.alpha = 0
         underLineLeadingConstraint.constant = 0
         underLineBar.updateConstraints()
         underLineBar.layoutIfNeeded()
-        // Hide it
+        // Hide the current tabs
         stackView.popOut(duration: 0.1) {[weak self] _ in
             guard let nonNullSelf = self else { return }
             
@@ -95,7 +111,7 @@ public class TapCardPhoneBarList: UIView {
             // Update it with the latest views
             views.forEach({ nonNullSelf.stackView.addArrangedSubview($0) })
             //nonNullSelf.stackView.layoutIfNeeded()
-            // Show it
+            // Show the new tabs
             nonNullSelf.stackView.popIn(duration:0.1)
         }
     }
@@ -136,6 +152,10 @@ extension TapCardPhoneBarList {
         themeBar()
     }
     
+    /**
+     Applies the correct theme for the underline view
+     - Parameter selectionValid: Indicates whether the current selected tab is a valid selection or not, as this affects the theme of the underline view
+     */
     private func themeBar(selectionValid:Bool = false) {
         let selectionThemePath:String = selectionValid ? "selected" : "unselected"
         underLineBar.tap_theme_progressTintColor = .init(keyPath: "\(themePath).underline.\(selectionThemePath).backgroundColor")
@@ -160,7 +180,9 @@ extension TapCardPhoneBarList:TapCardPhoneBarListViewModelDelegate {
     
     func calculatedSpacing() -> CGFloat {
         
+        // We need to make sure first if we have more than 1 tab inside the tab bar
         guard stackView.arrangedSubviews.count > 1 else { return 0 }
+        // Otherwise, we send the spacing which is the edge to edge spacing between two tabs
         return ((stackView.arrangedSubviews[1].frame.minX - stackView.arrangedSubviews[0].frame.maxX) / 2) + 2
         
     }
