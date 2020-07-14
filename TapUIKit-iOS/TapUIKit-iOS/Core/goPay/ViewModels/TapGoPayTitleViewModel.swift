6067a498344ec08da0aadd8bfd04c1ce6b9e82b5
O Rabie <o.rabie@tap.company>
2020-07-14 18:59:26 +0200

    1- goPay login tab bar title view

---
+++ TapUIKit-iOS/TapUIKit-iOS/Core/goPay/ViewModels/TapGoPayTitleViewModel.swift
@@ -0,0 +1,88 @@
+//
+//  TapGoPayTitleViewModel.swift
+//  TapUIKit-iOS
+//
+//  Created by Osama Rabie on 7/14/20.
+//  Copyright © 2020 Tap Payments. All rights reserved.
+//
+
+import Foundation
+import struct UIKit.CGRect
+
+/// Protocol to communicate between the parenr viewmodel (The bar list view model) and this view model
+internal protocol TapGoPayTitleViewModelDelegate {
+    /**
+     Inform the bar list view model that there is a title had been selected
+     - Parameter viewModel: The TapGoPayTitleViewModel related to the selected title
+     */
+    func titleIsSelected(with viewModel:TapGoPayTitleViewModel)
+}
+
+/// Protocol to communicate between the view controlled by this view model ad the view model itself
+internal protocol TapGoPayTitleViewDelegate {
+    /**
+     Asks the view to calclate its view bounds relative to its superview
+     - Returns: A rect represents the boundries of the associated view
+     */
+    func viewFrame() -> CGRect
+    /// Informs the attached view to reload itself to respond to certain changes in the view model
+    func reload()
+}
+
+
+/// View model that controls the actions and the ui of the card/phone bar inner icon
+@objc public class TapGoPayTitleViewModel:NSObject {
+    
+    // MARK:- Public normal swift variables
+    /// Represent the icon state
+    @objc public var titleStatus:TapCardPhoneIconStatus = .selected {
+        didSet{
+            // Update the attached view to reload itself based on the new status
+            viewDelegate?.reload()
+        }
+    }
+    
+    /// Represent the url for the image to be loaded inside this icon
+    @objc public var titleSegment:String = "" {
+        didSet{
+            // Update the attached view to set its text
+            viewDelegate?.reload()
+        }
+    }
+    
+    // MARK:- Private methods
+    
+    ///Delegate to communicate between the parenr viewmodel (The bar list view model) and this view model
+    internal var delegate:TapGoPayTitleViewModelDelegate?
+    
+    /// Delegae to communicate between the view controlled by this view model ad the view model itself
+    internal var viewDelegate:TapGoPayTitleViewDelegate?
+    
+    /// The attached view will call this method once it had been clicked by the user
+    internal func titleIsSelected() {
+        // We need to inform our view model delegate that a selection happened, so it can execute the needed logic
+        delegate?.titleIsSelected(with: self)
+    }
+    
+    
+    // MARK:- Public methods
+    
+    public static func == (lhs: TapGoPayTitleViewModel, rhs: TapGoPayTitleViewModel) -> Bool {
+        return lhs.titleSegment == rhs.titleSegment
+    }
+    
+    
+    /**
+     - Parameter tapCardPhoneIconStatus: Represent the icon state
+     - Parameter associatedCardBrand: Represent the associated payment brand this cell is linked to
+     - Parameter tapCardPhoneIconUrl: Represent the url for the image to be loaded inside
+     this icon
+     */
+    @objc public init(titleStatus: TapCardPhoneIconStatus = .selected, titleSegment:String) {
+        super.init()
+        defer{
+            self.titleStatus = titleStatus
+            self.titleSegment = titleSegment
+        }
+    }
+}
---
+++ TapUIKit-iOS/TapUIKit-iOS/Core/goPay/Views/TapGoPayTitleView.swift
@@ -0,0 +1,113 @@
+//
+//  TapGoPayTitleView.swift
+//  TapUIKit-iOS
+//
+//  Created by Osama Rabie on 7/14/20.
+//  Copyright © 2020 Tap Payments. All rights reserved.
+//
+
+import TapThemeManager2020
+
+/// Represent the icon cell inside the horizontal bar of go pay login options
+@objc public class TapGoPayTitleView: UIView {
+
+    // MARK:- Outlets
+    /// Represents the content view that holds all the subviews
+    @IBOutlet var contentView: UIView!
+    /// Represents the title to be displayed in this tab
+    @IBOutlet weak var titleLabel: UILabel!
+    /// The delegate that wants to hear from the view on new data and events
+    @objc public var viewModel:TapGoPayTitleViewModel? {
+        didSet{
+            reload()
+        }
+    }
+    /// Holds the last style theme applied
+    private var lastUserInterfaceStyle:UIUserInterfaceStyle = .light
+    private let themePath:String = "goPay"
+    
+    // Mark:- Init methods
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    /**
+     Seup the hint view according to the view model
+     - Parameter viewModel: The new required view model to attach the view to
+     */
+    @objc public func setup(with viewModel:TapGoPayTitleViewModel) {
+        self.viewModel = viewModel
+        self.viewModel?.viewDelegate = self
+    }
+    
+    // MARK:- Private methods
+    
+    /// Used as a consolidated method to do all the needed steps upon creating the view
+    private func commonInit() {
+        self.contentView = setupXIB()
+        configureWithStatus()
+    }
+    
+    /// Apply the needed logic to reload UI and localisations upon an order from the view model
+    private func configureWithStatus() {
+        applyTheme()
+        fetchData()
+    }
+    
+    /// Fetch the displayed title from the view model
+    private func fetchData() {
+        titleLabel.text = viewModel?.titleSegment ?? ""
+    }
+    
+    /// Instruct the view model the user did select this title
+    @IBAction func titleClicked(_ sender: Any) {
+        viewModel?.titleIsSelected()
+    }
+}
+
+
+extension TapGoPayTitleView:TapGoPayTitleViewDelegate {
+    func viewFrame() -> CGRect {
+        return self.frame
+    }
+    
+    func reload() {
+        configureWithStatus()
+    }
+}
+
+// Mark:- Theme methods
+extension TapGoPayTitleView {
+    /// Consolidated one point to apply all needed theme methods
+    public func applyTheme() {
+        matchThemeAttributes()
+    }
+    
+    /// Match the UI attributes with the correct theming entries
+    private func matchThemeAttributes() {
+        let status:TapCardPhoneIconStatus = viewModel?.titleStatus ?? .selected
+        
+        tap_theme_backgroundColor = .init(keyPath: "\(themePath).loginBar.backgroundColor")
+        titleLabel.tap_theme_font = .init(stringLiteral: "\(themePath).loginBar.title.\(status.themePath()).textFont")
+        titleLabel.tap_theme_textColor = .init(stringLiteral: "\(themePath).loginBar.title.\(status.themePath()).textColor")
+    }
+    
+    /// Listen to light/dark mde changes and apply the correct theme based on the new style
+    override public func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        TapThemeManager.changeThemeDisplay(for: self.traitCollection.userInterfaceStyle)
+        
+        guard lastUserInterfaceStyle != self.traitCollection.userInterfaceStyle else {
+            return
+        }
+        lastUserInterfaceStyle = self.traitCollection.userInterfaceStyle
+        applyTheme()
+    }
+}
+
