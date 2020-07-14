aeb4eafae48aaf6acde214c450bb9d87908914f4
O Rabie <o.rabie@tap.company>
2020-07-14 19:15:07 +0200

    1- Added the tab bar goPay login option to the example project

---
+++ TapUIKit-iOS/TapUIKit-Example/goPayTesting/GoPayLoginTabSegmentViewController.swift
@@ -0,0 +1,41 @@
+//
+//  GoPayLoginTabSegmentViewController.swift
+//  TapUIKit-Example
+//
+//  Created by Osama Rabie on 7/14/20.
+//  Copyright © 2020 Tap Payments. All rights reserved.
+//
+
+import UIKit
+import TapUIKit_iOS
+
+class GoPayLoginTabSegmentViewController: UIViewController {
+
+    
+    @IBOutlet weak var tapGoPayTitleTab: TapGoPayTitleView!
+    let tapGoPayTitleViewModel:TapGoPayTitleViewModel = .init(titleStatus: .selected, titleSegment: "Email")
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tapGoPayTitleTab.setup(with: tapGoPayTitleViewModel)
+    }
+    
+    @IBAction func statusChanged(_ sender: Any) {
+        guard let segment:UISegmentedControl = sender as? UISegmentedControl else { return }
+        
+        
+        switch segment.selectedSegmentIndex {
+        case 0:
+            tapGoPayTitleViewModel.titleStatus = .selected
+        case 1:
+            tapGoPayTitleViewModel.titleStatus = .otherSegmentSelected
+        default:
+            return
+        }
+        
+    }
+    
+
+}
