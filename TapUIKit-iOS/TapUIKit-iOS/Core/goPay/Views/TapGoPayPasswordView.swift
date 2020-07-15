3c04923ebd29b4bcc9135d28cee88eedb0c2a4e0
O Rabie <o.rabie@tap.company>
2020-07-15 16:33:52 +0200

    Create OTP and Password header view

---
+++ TapUIKit-iOS/TapUIKit-iOS/Core/goPay/Views/TapGoPayPasswordView.swift
@@ -0,0 +1,13 @@
+//
+//  TapGoPayPasswordView.swift
+//  TapUIKit-iOS
+//
+//  Created by Osama Rabie on 7/15/20.
+//  Copyright © 2020 Tap Payments. All rights reserved.
+//
+
+import UIKit
+
+@objc public class TapGoPayPasswordView: UIView {
+
+}
