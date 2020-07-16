f913975c7e23687db57f2fe1db3735abf168aacb
O Rabie <o.rabie@tap.company>
2020-07-16 13:43:16 +0200

    1- Added wrapper for OTP + GoPay login hint

---
+++ TapUIKit-iOS/TapUIKit-iOS/Core/goPay/Views/TapGoPayOTPView.swift
@@ -0,0 +1,21 @@
+//
+//  TapGoPayOTPView.swift
+//  TapUIKit-iOS
+//
+//  Created by Osama Rabie on 7/16/20.
+//  Copyright © 2020 Tap Payments. All rights reserved.
+//
+
+import UIKit
+
+class TapGoPayOTPView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
