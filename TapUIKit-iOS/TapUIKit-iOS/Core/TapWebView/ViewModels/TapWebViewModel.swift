0485fc04653ead677b056a8a3ce7ed315ac31630
O Rabie <o.rabie@tap.company>
2020-07-21 15:03:51 +0200

    1- Added internal protocol between the web view and the view model

--- TapUIKit-iOS/TapUIKit-iOS/Core/TapWebView/ViewModels/TapWebViewModel.swift
+++ TapUIKit-iOS/TapUIKit-iOS/Core/TapWebView/ViewModels/TapWebViewModel.swift
@@ -9,17 +9,37 @@
 import Foundation
 import WebKit
 
+
+internal protocol TapWebViewDelegate {
+    
+    func reloadWebView()
+    func load(with url:URL)
+    func stopLoading()
+    func loaderVisibility(changed to:Bool)
+}
+
 @objc public protocol TapWebViewModelDelegate {
     
     @objc func willLoad(request:URLRequest) -> WKNavigationActionPolicy
     @objc func didLoad(url:URL?)
-    @objc func didFail(with error:NSError,for url:URL?)
-    
+    @objc func didFail(with error:Error,for url:URL?)
+
 }
 
 @objc public class TapWebViewModel:NSObject {
- 
     var delegate:TapWebViewModelDelegate?
+    var viewDelegate:TapWebViewDelegate?
+    
+    @objc public func load(with url:URL) {
+        viewDelegate?.load(with: url)
+    }
+    
+    @objc public func reloadWebView() {
+        viewDelegate?.reloadWebView()
+    }
+    @objc public func stopLoading() {
+        viewDelegate?.stopLoading()
+    }
     
 }
 
@@ -27,11 +47,17 @@ import WebKit
 extension TapWebViewModel:WKNavigationDelegate {
     
     public func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
-        decisionHandler(delegate?.willLoad(request: navigationAction.request) ?? .allow)
+        
+        let decision = delegate?.willLoad(request: navigationAction.request) ?? .allow
+        
+        viewDelegate?.loaderVisibility(changed: decision == .allow)
+        
+        decisionHandler(decision)
     }
     
     public func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         delegate?.didLoad(url: webView.url)
+        viewDelegate?.loaderVisibility(changed: false)
     }
     
     public func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
