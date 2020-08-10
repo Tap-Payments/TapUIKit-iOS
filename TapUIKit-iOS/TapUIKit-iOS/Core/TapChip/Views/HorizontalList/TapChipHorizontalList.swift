7cede13749de76a827177bfd70fe4de951008fa9
O Rabie <o.rabie@tap.company>
2020-08-10 15:30:12 +0200

    Revert "1- Save card cell conflicts. 2- TaChipHorizontal conflcts"
    This reverts commit 1e0c60d705823b036e817c975a18749a8e9b18f0.

--- TapUIKit-iOS/TapUIKit-iOS/Core/TapChip/Views/HorizontalList/TapChipHorizontalList.swift
+++ TapUIKit-iOS/TapUIKit-iOS/Core/TapChip/Views/HorizontalList/TapChipHorizontalList.swift
@@ -39,8 +39,6 @@ import SimpleAnimation
     /// Represents the theme path to look for to UI this list
     private let themePath:String = "horizontalList"
     
-    private var myHeightAnchor:NSLayoutConstraint?
-    
     // Mark:- Init methods
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -73,8 +71,6 @@ import SimpleAnimation
         // First thing we load the XIB file and attach it to the curren view
         self.contentView = setupXIB()
         translatesAutoresizingMaskIntoConstraints = false
-        myHeightAnchor = heightAnchor.constraint(equalToConstant: 100)
-        self.addConstraint(myHeightAnchor!)
         // Second, theme it please!
         applyTheme()
         // Third, we do all the configurations needed as one time setup to our collection view
@@ -149,7 +145,7 @@ import SimpleAnimation
                     self?.headerViewHeightConstraint.constant = 0
                     self?.collectionViewToHederConstraint.priority = .defaultLow
                     self?.translatesAutoresizingMaskIntoConstraints = false
-                    self?.myHeightAnchor?.constant = 80
+                    self?.heightAnchor.constraint(equalToConstant: 80).isActive = true
                     self?.layoutIfNeeded()
                 },completion: { _ in
                     //self?.assignFlowLaout()
@@ -163,8 +159,7 @@ import SimpleAnimation
         UIView.animate(withDuration: 0.2, animations: { [weak self] in
             self?.headerViewHeightConstraint.constant = 30
             self?.collectionViewToHederConstraint.priority = .required
-            self?.translatesAutoresizingMaskIntoConstraints = false
-            self?.myHeightAnchor?.constant = 100
+            self?.heightAnchor.constraint(equalToConstant: 100).isActive = true
             self?.layoutIfNeeded()
             },completion: { [weak self] _ in
                 if self?.shouldShowHeader ?? false {
