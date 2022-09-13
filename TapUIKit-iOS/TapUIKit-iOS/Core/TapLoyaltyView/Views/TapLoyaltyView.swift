25ad7f698c2105fedb543c2fe8d03edfd3a4485d
O Rabie <o.rabie@tap.company>
2022-09-13 09:15:32 +0200

    Empty loyalty view and nib

---
+++ TapUIKit-iOS/TapUIKit-iOS/Core/TapLoyaltyView/Views/TapLoyaltyView.swift
@@ -0,0 +1,82 @@
+//
+//  TapLoyaltyView.swift
+//  TapUIKit-iOS
+//
+//  Created by Osama Rabie on 13/09/2022.
+//  Copyright © 2022 Tap Payments. All rights reserved.
+//
+
+import UIKit
+
+/// A view represents the loyalty points view used while paying
+class TapLoyaltyView: UIView {
+
+    /// The container view that holds everything from the XIB
+    @IBOutlet var containerView: UIView!
+    
+
+    
+    /// The path to look for theme entry in
+    private let themePath = "loyaltyView"
+    
+    // Mark:- Init methods
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    /// Used as a consolidated method to do all the needed steps upon creating the view
+    private func commonInit() {
+        self.containerView = setupXIB()
+        translatesAutoresizingMaskIntoConstraints = false
+        //handlerImageView.translatesAutoresizingMaskIntoConstraints = false
+        applyTheme()
+    }
+}
+
+
+
+// Mark:- Theme methods
+extension TapLoyaltyView {
+    /// Consolidated one point to apply all needed theme methods
+    public func applyTheme() {
+        matchThemeAttributes()
+    }
+    
+    /// Match the UI attributes with the correct theming entries
+    private func matchThemeAttributes() {
+        
+        /*titleLabel.tap_theme_font = .init(stringLiteral: "\(themePath).titleLabelFont")
+        titleLabel.tap_theme_textColor = .init(keyPath: "\(themePath).titleLabelColor")
+        
+        subtitleLabel.tap_theme_font = .init(stringLiteral: "\(themePath).subTitleLabelFont")
+        subtitleLabel.tap_theme_textColor = .init(keyPath: "\(themePath).subTitleLabelColor")
+        
+        merchantLogoContainerView.layer.tap_theme_cornerRadious = .init(keyPath: "\(themePath).merchantLogoCorner")
+        merchantLogoPlaceHolderView.tap_theme_backgroundColor = .init(keyPath: "\(themePath).merchantLogoPlaceHolderColor")
+        merchantLogoPlaceHolderInitialLabel.tap_theme_font = .init(stringLiteral: "\(themePath).merchantLogoPlaceHolderFont")
+        merchantLogoPlaceHolderInitialLabel.tap_theme_textColor = .init(keyPath: "\(themePath).merchantLogoPlaceHolderLabelColor")
+        
+        tap_theme_backgroundColor = .init(keyPath: "\(themePath).backgroundColor")
+        
+        // Push the title and the merchant header a bit if arabic is being used
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        subtitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        topSpaceBetweenMerchantNameAndTitleConstraint.constant += (TapLocalisationManager.shared.localisationLocale == "ar") ? 2 : 0*/
+        
+        layoutIfNeeded()
+    }
+    
+    /// Listen to light/dark mde changes and apply the correct theme based on the new style
+    override public func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        TapThemeManager.changeThemeDisplay(for: self.traitCollection.userInterfaceStyle)
+        applyTheme()
+    }
+}
