0a88709ff6253bb5acd25767dd362945965fd1eb
MahmoudAllam <m.allam@tap.company>
2023-05-24 16:00:40 +0300

    Apply theme

--- TapUIKit-iOS/TapUIKit-iOS/Core/TapCurrencyWidgetView/TapCurrencyWidgetView.swift
+++ TapUIKit-iOS/TapUIKit-iOS/Core/TapCurrencyWidgetView/TapCurrencyWidgetView.swift
@@ -19,11 +19,13 @@ class TapCurrencyWidgetView: UIView {
     @IBOutlet weak var amountLabel: UILabel!
     /// The currency View image view which displays currency to be used with this provider
     @IBOutlet weak var currencyImageView: UIImageView!
-    /// The hint label  which displays the CurrencyWidget hint
-    @IBOutlet weak var hintLabel: UILabel!
+    /// The message label  which displays the CurrencyWidget message
+    @IBOutlet weak var messageLabel: UILabel!
     /// The provider image view which displays the provider logo
     @IBOutlet weak var providerImageView: UIImageView!
 
+    /// The path to look for theme entry in
+    private let themePath = "CurrencyWidget"
     
     // Mark:- Init methods
     override init(frame: CGRect) {
@@ -40,6 +42,7 @@ class TapCurrencyWidgetView: UIView {
     /// Used as a consolidated method to do all the needed steps upon creating the view
     private func commonInit() {
         self.containerView = setupXIB()
+        applyTheme()
         translatesAutoresizingMaskIntoConstraints = false
     }
     
@@ -47,4 +50,51 @@ class TapCurrencyWidgetView: UIView {
     @IBAction func confirmClicked(_ sender: Any) {
     }
     
+}
+
+// Mark:- Theme methods
+extension TapCurrencyWidgetView {
+    /// Consolidated one point to apply all needed theme methods
+    public func applyTheme() {
+        matchThemeAttributes()
+    }
+    
+    /// Match the UI attributes with the correct theming entries
+    private func matchThemeAttributes() {
+        
+        let amountLabelThemePath = "\(themePath).amountLabel"
+        amountLabel.tap_theme_font = .init(stringLiteral: "\(amountLabelThemePath).font")
+        amountLabel.tap_theme_textColor = .init(keyPath: "\(amountLabelThemePath).color")
+        
+        let messageLabelThemePath = "\(themePath).messageLabel"
+        messageLabel.tap_theme_font = .init(stringLiteral: "\(messageLabelThemePath).font")
+        messageLabel.tap_theme_textColor = .init(keyPath: "\(messageLabelThemePath).color")
+        
+        let backgroundThemePath = "\(themePath).background"
+        containerView.tap_theme_backgroundColor = .init(keyPath: "\(backgroundThemePath).color")
+        containerView.layer.tap_theme_cornerRadious = ThemeCGFloatSelector.init(keyPath: "\(backgroundThemePath).cornerRadius")
+        containerView.layer.masksToBounds = true
+        containerView.clipsToBounds = false
+        containerView.layer.shadowOpacity = 1
+        containerView.layer.tap_theme_shadowRadius = .init(keyPath: "\(backgroundThemePath).shadow.blurRadius")
+        containerView.layer.tap_theme_shadowColor = .init(keyPath: "\(backgroundThemePath).shadow.color")
+        
+        let confirmButtonThemePath = "\(themePath).confirmButton"
+        
+        confirmButton.tap_theme_setTitleColor(selector: .init(keyPath: "\(confirmButtonThemePath).titleFontColor"), forState: .normal)
+        confirmButton.tap_theme_tintColor = .init(keyPath: "\(confirmButtonThemePath).titleFontColor")
+        confirmButton.titleLabel?.tap_theme_font = .init(stringLiteral: "\(confirmButtonThemePath).titleFont")
+        confirmButton.layer.tap_theme_cornerRadious = ThemeCGFloatSelector.init(keyPath: "\(confirmButtonThemePath).cornerRadius")
+        confirmButton.tap_theme_backgroundColor = .init(keyPath: "\(confirmButtonThemePath).backgroundColor")
+        
+
+        layoutIfNeeded()
+    }
+    
+    /// Listen to light/dark mde changes and apply the correct theme based on the new style
+    override public func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        TapThemeManager.changeThemeDisplay(for: self.traitCollection.userInterfaceStyle)
+        applyTheme()
+    }
 }
